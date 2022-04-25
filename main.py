66ab01faab9f6eafa666450e2a19075d6d5a9cb2
Evgen <ver68ni86@gmail.com>
2022-04-25 21:53:41 +0300

    add

--- main.py
+++ main.py
@@ -49,3 +49,30 @@ print(a)
 a //=2
 print(a)
 0.0
+2&3
+2
+2|3
+3
+12 ^ 15
+3
+~1
+-2
+4 << 6
+256
+8 >> 12
+0
+a = 10
+a > 7 and 12 > 4
+True
+a > 7 or 12 < 4
+True
+a = not(0)
+colour=['red', 'blue', 'broun']
+'black' in colour
+False
+'black' not in colour
+True
+10 is 10
+True
+15 is not 20
+True
