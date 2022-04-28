09eed858e9d1062cc02ab6d6765d626bf3289708
Evgen <ver68ni86@gmail.com>
2022-04-28 20:29:21 +0300

    calc

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+x = float(input('введите число 1 = '))
+y = float(input('введите число 2 = '))
+z = input('введите значение ')
+
+if z == '+':
+    print(x + y)
+elif z == '-':
+    print(x - y)
+elif z== '*':
+    print(x * y)
+elif z == '/':
+    print(x / y)
 
 
 
